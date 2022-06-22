38d7ba11f3ba4201fc6eb91ec9cb9487f11063d3
Isabelle Victoria <93364960+isabvictoriaps@users.noreply.github.com>
2022-06-21 21:38:45 -0300

    integrando back-end na tela de cadastro do freguês

--- js/cadasto.js
+++ js/cadasto.js
@@ -1,33 +1,3 @@
-/*NAVBAR*/
-
-/*PESQUISAR*/
-let searchForm = document.querySelector(".search-form");
-
-document.querySelector("#search-btn").onclick = () => {
-  console.log("oi");
-  searchForm.classList.toggle("active");
-};
-
-/* MEU CARRINHO */
-
-let carrinhoDiv = document.querySelector(".carrinhoFregues");
-
-document.querySelector("#botaoCarrinho").onclick = () => {
-  console.log("oi");
-  carrinhoDiv.classList.toggle("active");
-};
-
-/* MEU PERFIL */
-
-let perfilDiv = document.querySelector(".perfilFregues");
-
-document.querySelector("#botao-perfil").onclick = () => {
-  console.log("oi");
-  perfilDiv.classList.toggle("active");
-};
-
-/* ADICIONAR NO CARRINHO */
-
 /* INTEGRAÇÃO BACK-END */
 
 const formulario = document.querySelector("form");
